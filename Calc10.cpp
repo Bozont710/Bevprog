a588ccf33081e69cf31987b1e870e68ad6f887d0
Bozont710 <90345719+Bozont710@users.noreply.github.com>
2021-11-02 18:05:44 +0100

    Update Calc10.cpp

--- Calc10.cpp
+++ Calc10.cpp
@@ -1,212 +1,370 @@
-
-//
-// This is example code from Chapter 6.7 "Trying the second version" of
-// "Software - Principles and Practice using C++" by Bjarne Stroustrup
-//
-
 /*
-    This file is known as calculator02buggy.cpp
-    I have inserted 5 errors that should cause this not to compile
-    I have inserted 3 logic errors that should cause the program to give wrong results
-    First try to find an remove the bugs without looking in the book.
-    If that gets tedious, compare the code to that in the book (or posted source code)
-    Happy hunting!
+    The first working version of Stroustrup's calculator.
+    Written by Renátó Besenczi based on the book BS, "Programming: Principles and Practice using C++, Second Edition"
+    See chapter 6 and chapter 7 of book: https://stroustrup.com/programming.html
+    © 2020 Renátó Besenczi, according to CC0. Questions: renato.besenczi@gmail.com
+    Input from cin; output to cout. Input comes from cin through the Token_stream called ts.
+    Grammar:
+    
+    Calculation:
+        Statement
+        Print
+        Quit
+        Calculation Statement
+    Print:
+        ;
+    Quit:
+        q
+    Statement:
+        Declaration
+        Expression
+    Declaration:
+        "let" Name "=" Expression
+    Name:
+        string literal
+    Expression:
+        Term
+        Expression + Term
+        Expression – Term
+    Term:
+        Primary
+        Term * Primary
+        Term / Primary
+        Term % Primary
+    Primary:
+        Number
+        ( Expression )
+        – Primary
+        + Primary
+    Number:
+        floating-point-literal
 */
 
-#include "std_lib_facilities.h"
+#include "std_lib_facilities.h" //Include usual header
 
-
-
+// Some symbolic constants for clarity
 constexpr char number = '8';
 constexpr char quit = 'x';
-constexpr char print = '=';
+constexpr char print = ';';
+constexpr char name = 'a';
+constexpr char let = 'L';
+constexpr char result = '=';
+const string declkey = "let"; //constexpr string since C++20 only
 
+//function declarations
+double expression();
+double primary();
+double term();
+double declaration();
+double statement();
 
-//------------------------------------------------------------------------------
-
-class Token{
+// Variable ///////////////////////
+class Variable {
 public:
-    char kind;        // what kind of token
-    double value;     // for numbers: a value 
-    Token(char ch)    // make a Token from a char
-        :kind(ch), value(0) { }
-    Token(char ch, double val)     // make a Token from a char and a double
-        :kind(ch), value(val) { }
+    string name;
+    double value;
 };
 
-//------------------------------------------------------------------------------
+vector<Variable> var_table;
 
+bool is_declared(string var)
+{
+    for (const auto& v : var_table)
+        if (v.name == var) return true;
+    return false;
+}
+
+double define_name (string var, double val)
+{
+    if (is_declared(var)) error(var, " declared twice");
+    var_table.push_back(Variable{var,val});
+    return val;
+}
+
+double get_value(string s)
+{
+    for(const auto& v : var_table)
+        if (v.name == s) return v.value;
+    error("get: undefined variable", s);
+    return -1;
+}
+
+void set_value(string s, double d)
+{
+    for (auto& v : var_table)
+        if(v.name == s){
+            v.value = d;
+            return;
+        }
+    error("set: undefined variable", s);
+}
+// Variable end ///////////////////////
+
+// Token ///////////////////////
+class Token {
+public:
+    char kind;
+    double value;
+    string name;
+    Token(): kind(0) {}
+    Token(char ch): kind(ch), value(0) {}
+    Token(char ch, double val): kind(ch), value(val) {}
+    Token(char ch, string n): kind(ch), name(n) {}
+};
+// Token end ///////////////////////
+
+// Token_stream ///////////////////////
 class Token_stream {
 public:
-    Token_stream();   // make a Token_stream that reads from cin
-    Token get();      // get a Token (get() is defined elsewhere)
-    void putback(Token t);    // put a Token back
+    Token_stream();
+    void putback(Token t);
+    Token get();
+    void ignore(char c);
 private:
-    bool full;        // is there a Token in the buffer?
-    Token buffer;     // here is where we keep a Token put back using putback()
+    bool full;
+    Token buffer;
 };
 
-//------------------------------------------------------------------------------
+Token_stream::Token_stream() :full(false), buffer(0) {}
 
-// The constructor just sets full to indicate that the buffer is empty:
-Token_stream::Token_stream()
-    :full(false), buffer(0)    // no Token in buffer
-{
-}
-
-//------------------------------------------------------------------------------
-
-// The putback() member function puts its argument back into the Token_stream's buffer:
 void Token_stream::putback(Token t)
 {
-    if (full) error("putback() into a full buffer");
-    buffer = t;       // copy t to buffer
-    full = true;      // buffer is now full
+    if (full) error("putback() into full buffer");
+    buffer = t;
+    full = true;
 }
-
-//------------------------------------------------------------------------------
 
 Token Token_stream::get()
 {
-    if (full) {       // do we already have a Token ready?
-        // remove token from buffer
+    if (full)
+    {
         full = false;
         return buffer;
     }
 
     char ch;
-    cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+    cin >> ch;
 
-    switch (ch) {
-    case print:    // for "print"
-    case quit:    // for "quit"
-    case '(': 
-    case ')': 
-    case '+': 
-    case '-': 
-    case '*': 
-    case '/':
-        return Token(ch);        // let each character represent itself
-    case '.':
-    case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '8': case '9':
+    switch (ch)
     {
-        cin.putback(ch);         // put digit back into the input stream
-        double val;
-        cin >> val;              // read a floating-point number
-        return Token(number, val);   // let '8' represent "a number"
-    }
-    default:
-        error("Bad token");
+        case print:
+        case quit:
+        case '(':
+        case ')':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '%':
+        case '=':
+            return Token(ch);
+        case '.':
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        {
+            cin.putback (ch);
+            double val;
+            cin >> val;
+            return Token(number, val);
+        }
+        default: 
+        {
+            if (isalpha(ch))
+            {
+                string s;
+                s += ch;
+                while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
+                cin.putback(ch);
+                if (s == declkey) return Token{let};
+                else if (is_declared(s))
+                    return Token(number, get_value(s));
+                return Token{name,s};
+            }
+            error("Bad token");
+        }
         return 0;
     }
 }
 
-//------------------------------------------------------------------------------
+void Token_stream::ignore(char c)
+{
+    if (full && c == buffer.kind)
+    {
+        full = false;
+        return;
+    }
 
-Token_stream ts;        // provides get() and putback() 
+    full = false;
 
-//------------------------------------------------------------------------------
+    char ch = 0;
+    while (cin>>ch)
+        if (ch==c) return;
+}
+// Token_stream end ///////////////////////
+Token_stream ts;
 
-double expression();    // declaration so that primary() can call expression()
+void clean_up_mess()
+{
+    ts.ignore(print);
+}
 
-//------------------------------------------------------------------------------
+// Logically separate calculate() from the usual "boiler plate" of main 
+void calculate()
+{
+    while (cin)
+    try {
+        Token t = ts.get();
+        while (t.kind == print) t = ts.get();
+        if (t.kind == quit) return;
+        ts.putback(t);
+        cout << result << statement() << endl;
+    }
+    catch (exception& e) {
+        cerr << e.what() << endl;
+        clean_up_mess();
+    }
+}
 
-// deal with numbers and parentheses
+int main() 
+try {
+
+    cout << "Welcome to our simple calculator.\n";
+    cout << "Please enter expressions using floating-point numbers.\n";
+    cout << "You may use '+' '-' '*' '/' '%' operators.\n";
+    cout << "Use 'x' to exit and ';' to print.\n";
+
+    define_name("pi", 3.1415926535);
+    define_name("e", 2.7182818284);
+    
+    calculate();
+
+    return 0;
+
+} catch (exception& e){
+    cerr << e.what() << endl;
+    return 1;
+} catch (...) {
+    cerr << "exception\n";
+    return 2;
+}
+
+//Functions mapping grammar rules //////////////////////////
 double primary()
 {
     Token t = ts.get();
-    switch (t.kind) {
-    case '(':    // handle '(' expression ')'
+    switch (t.kind)
     {
-        double d = expression();
-        t = ts.get();
-        if (t.kind != ')') error("')' expected");
+        case '(':
+        {
+            double d = expression();
+            t = ts.get();
+            if (t.kind != ')') error("')' expected");
             return d;
+        }
+        case number:
+            return t.value;
+        case '-':
+            return - primary();
+        case '+':
+            return primary();
+        default:
+            error("primary expected");
     }
-    case number:            // we use '8' to represent a number
-        return t.value;  // return the number's value
-    default:
-        error("primary expected");
-        return 0;
-    }
+    return -1;
 }
-//------------------------------------------------------------------------------
-// deal with *, /, and %
+
 double term()
 {
     double left = primary();
-    Token t = ts.get();        // get the next token from token stream
-    while (true) {
-        switch (t.kind) {
-        case '*':
-            left *= primary();
-            t = ts.get();
-            break;
-        case '/':
+    Token t = ts.get();
+    while(true)
+    {
+        switch (t.kind)
         {
-            double d = primary();
-            if (d == 0) error("divide by zero");
-            left /= d;
-            t = ts.get();
-            break;
-        }
-        default:
-            ts.putback(t);     // put t back into the token stream
-            return left;
+            case '*':
+                left *= primary();
+                t = ts.get();
+                break;
+            case '/':
+            {
+                double d = primary();
+                if (d == 0) error("divide by zero");
+                left /= d;
+                t = ts.get();
+                break;
+            }
+            case '%':
+            {
+                double d = primary();
+                if (d == 0) error("%: divide by zero");
+                left = fmod (left, d);
+                t = ts.get();
+                break;
+                /* //or you can use narrow cast check
+                int i1 = narrow_cast<int>(left);
+                int i2 = narrow_cast<int>(primary());
+                if (i2 == 0) error ("%: Zero oszto");
+                left = i1 % i2;
+                t = ts.get();
+                break;
+                */
+            }
+            default:
+                ts.putback(t);
+                return left;
         }
     }
 }
-//------------------------------------------------------------------------------
-// deal with + and -
+
 double expression()
 {
-    double left = term();      // read and evaluate a Term
-    Token t = ts.get();        // get the next token from token stream
-    while (true) {
-        switch (t.kind) {
-        case '+':
-            left += term();    // evaluate Term and add
-            t = ts.get();
-            break;
-        case '-':
-            left -= term();    // evaluate Term and subtract
-            t = ts.get();
-            break;
-        default:
-            ts.putback(t);     // put t back into the token stream
-            return left;       // finally: no more + or -: return the answer
+    double left = term();
+    Token t = ts.get();
+    while (true)
+    {
+        switch(t.kind)
+        {
+            case '+':
+                left += term();
+                t = ts.get();
+                break;
+            case '-':
+                left -= term();
+                t = ts.get();
+                break;
+            default:
+                ts.putback(t);
+                return left;
         }
     }
 }
-//------------------------------------------------------------------------------
-int main()
-try
+
+double declaration()
 {
+    Token t = ts.get();
+    if (t.kind != name) error("name expected in declaration");
+    string var_name = t.name;
 
-    cout << "Welcome to our simple calculator.\n" ;
-    cout << "Please enter expressions using floating-point numbers.\n";
-    cout << "You can use '+' '-' '/' '*' operators and pharenthesis as well, use '=' to print and 'x' to exit the program.\n";
+    Token t2 = ts.get();
+    if (t2.kind != '=') error("= missing in declaration of ", var_name);
 
-    
-    double val = 0;
-    while (cin) {
-        Token t = ts.get();
-        if (t.kind == quit){ break;} // 'q' for quit
-        if (t.kind == print) {       // ';' for "print now"
-            cout << "=" << val << '\n';
-        }
-        else
+    double d = expression();
+    define_name(var_name, d);
+    return d;
+}
+
+double statement()
+{
+    Token t = ts.get();
+    switch(t.kind)
+    {
+        case let:
+            return declaration();
+        default:
             ts.putback(t);
-        val = expression();
+            return expression();
     }
 }
-catch (exception& e) {
-    cerr << "error: " << e.what() << '\n';
-    return 1;
-}
-catch (...) {
-    cerr << "Oops: unknown exception!\n";
-    return 2;
-}
-//------------------------------------------------------------------------------
+//Functions mapping grammar rules end //////////////////////////
+
+// Program end!
